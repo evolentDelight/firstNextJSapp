cdac4e616337e27e85f4aae71687c2c438654691
evolentDelight <evolentDelight@outlook.com>
2020-07-06 15:06:51 -0700

    Post publish

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,14 +1,31 @@
+import Head from 'next/head'
 import Layout from "../../components/layout";
+import Date from '../../components/date'
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post( {postData} ) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date}/>
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
@@ -22,7 +39,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
